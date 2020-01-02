54e5fd8620b0ab1c83e14f088debf75b4311284c
Neon22 <mschafer@wireframe.biz>
2020-01-03 10:41:55 +1300

    comments and SttchStitch class name change
    Added comments. not sure all are accurate.
    
    Changes StitchSitch class to Connected_Stitch

--- pattern_parser.py
+++ pattern_parser.py
@@ -18,7 +18,7 @@ whitespace = re.compile(r'^\s+$')
 flatten = lambda l: [item for sublist in l for item in sublist]
 
 class StitchPattern(object):
-	""" Holds the parsed pattern of Stitches 
+    """ Holds the parsed pattern of Stitches 
     """
     def __init__(self, pattern_type, parsed_pattern, name):
         self.pattern = parsed_pattern
@@ -28,12 +28,12 @@ class StitchPattern(object):
         self.name = name
 
     def expand(self):
-		""" return fully expanded rows of all stitches
+        """ return fully expanded rows of all stitches
         """
         return [self.expand_row(i) for i in range(len(self.pattern))]
 
     def count_stitches(self, rownum):
-		""" count stitches in each row
+        """ count stitches in each row
             - deal with chains, slips and skips
         """
         stitch_list = self.expand_row(rownum)
@@ -45,6 +45,7 @@ class StitchPattern(object):
                 chcount = 0
             elif st == 'ch':
                 chcount += 1
+                # inc count by 1 for every 3 ch stitches
                 if chcount >= 3:
                     count += 1
             else:
@@ -53,11 +54,11 @@ class StitchPattern(object):
         return count
 
     def expand_stitchgroup(self, stitchgroup):
-		" return flat list of all stitches in a stitchgroup "
+        " return flat list of all stitches in a stitchgroup "
         return flatten([self.expand_stitch(s) for s in stitchgroup])
 
     def expand_stitch(self, stitch):
-		""" return list of individual stitches.
+        """ return list of individual stitches.
             special case chain stitches of length 3
         """
         if stitch == ('ch', 3):
@@ -66,22 +67,22 @@ class StitchPattern(object):
         return [st for x in range(ct)]
 
     def expand_row(self, rownum):
-		" return flat list of all stitches in the row "
+        " return flat list of all stitches in the row "
         # cache already expanded rows to avoid reworking them
         if rownum not in self.expand_row_helper:
             self.expand_row_helper[rownum] = self._expand_row(rownum)
         return self.expand_row_helper[rownum]
 
     def _expand_row(self, rownum):
-		""" expand stitch list for all stitchgroups in a row 
+        """ expand stitch list for all stitchgroups in a row 
             - return flat list of stitches
         """
-		# is this the first stitch in the row
+        # is this the first stitch in the row
         if rownum == 0:
             prev_stitch_num = self.start_stitches
         else:
             prev_stitch_num = self.count_stitches(rownum - 1)
-		#
+        #
         row = self.pattern[rownum]
         if len(row) == 1:
             return flatten([self.expand_stitchgroup(s) for s in row[0]])
@@ -100,23 +101,24 @@ class StitchPattern(object):
 
 	# called by viz
     def stitch_map(self):
-		" "
+        " "
         self.stitch_num_id = 1
         return self.stitch_map_row_accuum(0, [self.foundation_row_map()])
 
     def stitch_map_row_accuum(self, rownum, map_so_far):
-		" "
-		# are we done yet
+        " "
+	# are we done yet
         if rownum == len(self.pattern):
             return map_so_far
-		#
+        #
         row = self.pattern[rownum]
         prev_row = map_so_far[-1]
         prev_row_idx = 0
         new_map_row = []
+        # special case row witha single stitch
         short_row = 0 if len(row) > 1 else 1
         for i in range(len(row[0]) - short_row):
-			# expand one stitchgroup at a time gathering stitches 
+            # expand one stitchgroup at a time gathering stitches 
             stitches = self.expand_stitchgroup(row[0][i])
             for j in range(len(stitches)):
                 if stitches[j] == 'sk':
@@ -127,14 +129,15 @@ class StitchPattern(object):
                 else:
                     prev = prev_row[-1]
                 if stitches[j] == 'ch' or stitches[j] == 'sl':
-					# chain and slip stitches are not connected to a prev row
+                    # chain and slip stitches are not connected to a prev row
                     bottom = None
                 else:
                     bottom = prev_row[prev_row_idx]
-				# store the stitch along with prev and sitch it is stitched into
-                new_map_row.append(StitchStitch(self.get_stitch_id(), stitches[j], prev, bottom))
+                    # store the stitch along with prev and sitch it is stitched into
+                new_map_row.append(Connected_stitch(self.get_stitch_id(), stitches[j], prev, bottom))
             prev_row_idx += 1 
         if len(row) == 1:
+            # special case single stitch rows
             last_stitchgroup = row[0][-1]
         else:
             #! todo fix me only handles one stitch after repeat
@@ -169,7 +172,7 @@ class StitchPattern(object):
 
 	# called by stitch_map (viz)
     def foundation_row_map(self):
-		""" Initial stitch in the pattern.
+        """ Initial stitch in the pattern.
             return magic loop for 'rounds' or nothing 
         """
         if self.pattern_type == 'round':
@@ -181,7 +184,7 @@ class StitchPattern(object):
         return retval
 
     def viz(self):
-		""" Build a dotmap for graphviz 
+        """ Build a dotmap for graphviz 
             but also constructs the connected_stitch structures
         """
         flatmap = flatten(self.stitch_map())
@@ -202,9 +205,9 @@ class StitchPattern(object):
 
 
 class Connected_stitch(object):
-	""" Each Stitch points to previous
+    """ Each Stitch points to previous
          and bottom 
-		 - which is the location of connection on previous row
+        - which is the location of connection on previous row
     """
     def __init__(self, idnum, stitch, prev=None, bottom=None):
         self.id = idnum
@@ -213,7 +216,7 @@ class Connected_stitch(object):
         self.bottom = bottom
 
 def parse_stitch(stitch):
-	""" For each stitch in the pattern file 
+    """ For each stitch in the pattern file 
         - ?
     """
     result = []
@@ -224,14 +227,14 @@ def parse_stitch(stitch):
 
 
 def parse_pattern(filename):
-	""" extract the lines from file and
+    """ extract the lines from file and
         return list of parsed stitches
     """
     with open(filename, 'r') as f:
         lines = f.readlines()
         pattern_type = lines[0].strip()
         name = filename.split('.')[0]
-		# for each line parse all the stitches
+        # for each line parse all the stitches
         #  - split on * and for each , separated stitch
         parsed_stitches = [[[parse_stitch(sg.strip()) for sg in tris.split(',') if len(sg.strip()) > 0] for tris in l.strip().split('*')] for l in lines[1:]]
         return (name, pattern_type, parsed_stitches)
@@ -241,12 +244,12 @@ if __name__ == "__main__":
         arg1 = sys.argv[1]
     else:
         arg1 = 'example_pattern.txt'
-	# parse the written pattern
+    # parse the written pattern
     name, ptype, rows = parse_pattern(arg1)
-	# construct a StitchPattern from the parsed stitches
+    # construct a StitchPattern from the parsed stitches
     sp = StitchPattern(ptype, rows, name)
+    # report
     print sp.expand()
-	# report
     for row in rows:
         print "row:"
         if len(row) == 1:
@@ -263,5 +266,5 @@ if __name__ == "__main__":
             print "\tjoin:"
             for stitch in row[-1]:
                 print "\t\tstitch:", stitch
-	# Visualize it (also makes structure)
+    # Visualize it (also makes structure)
     sp.viz()
